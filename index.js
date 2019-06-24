0969f53536db823d449b773ba9505cfd9da5e9ba
Ellison Rio <rio.ellison@gmail.com>
2019-06-24 12:49:20 -0400

    index.js + gitignore

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const parser = require("body-parser");
+
+const usersController = require("./controllers/users")
+
+const app = express();
+
+const cors = require("cors")
+
+app.use(cors())
+
+app.use(parser.urlencoded({ extended: true}));
+
+app.use(parser.json());
+
+app.get("/", (req, res) => {
+    res.redirect("/api/users")
+});
+//or to page with everyone's starting five?
+
+app.use("/api/users", usersController);
+
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
