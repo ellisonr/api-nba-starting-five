96225f54cc8fc46d93fe434a6e66b5f4bead493e
Ellison Rio <rio.ellison@gmail.com>
2019-06-24 12:57:06 -0400

    basics in users controller

---
+++ controllers/users.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../db/models/User");
+
+router.get("/", (req, res) => {
+    User.find({}).then(users => res.json(users));
+});
+//get all users
+
+router.get("/:name", (req, res) => {
+    User.find({ name: req.params.name})
+    .then(user => res.json(user));
+});
+//get user by name
+
+router.post("/", (req, res) => {
+    User.create(req.body)
+    .then(user => res.json(user));
+});
+//create user
+
+router.delete("/:name", (req, res) => {
+    User.findOneAndDelete({ name: req.params.name})
+    .then(user => {
+        res.json(user);
+    });
+});
+//delete user by name
