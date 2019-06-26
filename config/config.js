f1ea4a4d1a2ff65f20f57c119064bbdbb7062324
Ellison Rio <rio.ellison@gmail.com>
2019-06-26 13:23:17 -0400

    config.js fix

--- config/config.js
+++ config/config.js
@@ -1,16 +1,17 @@
 makeid = () => {
-	var result = ‘’;
-	var characters = ‘ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789’;
+	var result = '';
+	var characters =
+		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	var charactersLength = characters.length;
 	for (var i = 0; i < 10; i++) {
 		result += characters.charAt(Math.floor(Math.random() * charactersLength));
 	}
 	return result;
- }
- 
- module.exports = {
+};
+
+module.exports = {
 	jwtSecret: makeid(),
 	jwtSession: {
-		session: false
-	}
- }
+		session: false,
+	},
+};
